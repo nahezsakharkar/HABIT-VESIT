e777a20ce429edde4e63351f7de3b1f59809ff41
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-09-11 23:00:03 +0530

    footer location changed

---
+++ src/components/ProgramsComponents/AllPrograms/AllPrograms.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable react/no-unescaped-entities */
+import programsData from "./../../../assets/data/programs.json";
+import "./style.css";
+
+export default function AllPrograms() {
+  return (
+    <div>
+      <div className="list">
+        {programsData.programs.map((item) => (
+          <div key={item.color}>
+            <a data-aos="fade-up" className="work-tile">
+              <div className="background">
+                <img
+                  src={`src/assets/images/${item.image}`}
+                  alt="HABIT Program Image"
+                />
+              </div>
+              <div className="foreground">
+                <div
+                  className="brand-colour"
+                  style={{ backgroundColor: item.color }}
+                ></div>
+                <div className="top">
+                  <div className="stat">{item.stat}</div>
+                </div>
+                <div className="bottom">
+                  <div className="type">{item.type}</div>
+                  <div className="client">{item.client}</div>
+                </div>
+              </div>
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/Programs/Programs.jsx
+++ src/pages/Programs/Programs.jsx
@@ -1,10 +1,12 @@
 import Heading from "../../components/ProgramsComponents/Heading/Heading";
+import AllPrograms from "../../components/ProgramsComponents/AllPrograms/AllPrograms";
 import "./style.css";
 
 export default function Programs() {
   return (
     <div className="container inner-container">
       <Heading />
+      <AllPrograms />
     </div>
   );
 }

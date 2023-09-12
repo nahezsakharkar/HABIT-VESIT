2c84674e7103473f4244531429bfbcb5ac0e88a6
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-09-12 11:12:02 +0530

    hero completed

--- src/components/HomeComponents/Hero.jsx
+++ src/components/HomeComponents/Hero.jsx
@@ -1,40 +1,39 @@
-import React from "react";
 import "./Hero.css";
+
 const Hero = () => {
   return (
-    <>
-      <div className="hero-container">
-        <div className="hero-container-col">
+    <div className="hero-container">
+      <div className="hero-container-col">
+        <div className="hero-img-container">
+          <div className="bulb">
+            <img src="src\assets\images\HERO.png" alt="Hero Icon" />
+          </div>
+          {/* <div className="wavedot">
+            <img src="src\assets\images\polkadots.png" alt="Hero Icon" />
+          </div> */}
           <div className="hero-img-circle"></div>
-          <div className="hero-img-box">
-            <div className="hero-img-box">
-            <div className="hero-img-box-con">
-              <img src="src\assets\images\HERO.png" alt="Hero Icon" />
-            </div>
-            </div>
-           
-          </div>
-        </div>
-        <div className="hero-container-col">
-          <div className="hero-title">
-            HABIT <br /> INCUBATION CENTRE
-          </div>
-          <div className="quote-box">
-            <p className="quote">
-              Nurturing ideas is akin to tending a delicate sapling, providing
-              the care and nurturing necessary for their growth and blossoming.
-              It involves the gentle cultivation of innovative concepts and
-              ensuring they have the fertile ground they need to thrive
-            </p>
-          </div>
-          <div className="explore-btn-box">
-            <button class="glow-on-hover" type="button">
-              Explore...
-            </button>
-          </div>
         </div>
       </div>
-    </>
+      <div className="hero-container-col right">
+        <div className="hero-title">
+          <h3>HABIT</h3>
+          <h5>INCUBATION CENTRE</h5>
+        </div>
+        <div className="quote-box">
+          <p className="quote">
+            Nurturing ideas is akin to tending a delicate sapling, providing the
+            care and nurturing necessary for their growth and blossoming. It
+            involves the gentle cultivation of innovative concepts and ensuring
+            they have the fertile ground they need to thrive
+          </p>
+        </div>
+        <div className="explore-btn-box">
+          <button className="glow-on-hover" type="button">
+            Explore...
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,10 +4,10 @@ import "./style.css";
 
 function Home() {
   return (
-    <>
+    <main>
       <Hero />
       <Leader />
-    </>
+    </main>
   );
 }
 

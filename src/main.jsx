ac67c0da7327a5e966a0594fa2bb2094289c1a3c
NAHEZ SAKHARKAR <nahezsakharkar@gmail.com>
2023-09-12 21:47:56 +0530

    yeah

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/HABIT-VESIT/"}>
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>

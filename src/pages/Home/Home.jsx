b9dc4e3c00c0418b2791218ecea10aeb6c6f833c
Krushna Jadhav <jadhavkrushna449@gmail.com>
2023-09-12 10:18:19 +0530

    home page modified!

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
+import Hero from "../../components/HomeComponents/Hero";
+import Leader from "../../components/HomeComponents/Leader";
 import "./style.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Hero />
+      <Leader />
+    </>
+  );
 }
 
 export default Home;
